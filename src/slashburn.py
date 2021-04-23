c1b0ed1b3e82019eab828531b5da8573e7aa160f
Jacob Frank <jfrank21@amherst.edu>
2021-04-22 20:16:51 -0400

    Updated Comments

--- src/slashburn.py
+++ src/slashburn.py
@@ -27,7 +27,7 @@ def scc(nodes=[], edges=[]):
     nodes = nodes
     edges = edges
     n = len(nodes)
-    for u, v in edges:
+    for u, v in edges: # Is this necessary for undirected graphs?
         alists[u].append(v)
         rlists[v].append(u)
     t = 0
@@ -37,21 +37,23 @@ def scc(nodes=[], edges=[]):
         rvisited[s] = True
         stack = [s]
         while len(stack):
-            i = stack[-1]
+            i = stack[-1] # negative numbers mean indexing from the end of the list. -1 is the last element in stack
             sink = True
             for j in rlists[i]:
                 if not rvisited[j]:
                     rvisited[j] = True
-                    stack.append(j)
+                    stack.append(j) #Adding all direct neighbors for node at i
                     sink = False
                     break
             if sink:
-                t += 1
+                t += 1 #not sure what t keeps track of, never resets to 0 at each node s
                 f[i] = t
                 stack.pop()
+    #for key in f: #builds list such that (0,0) (1,1), etc are key value pairs
+     #   print(key, f[key])  
     for key in f:
         r[f[key]] = key
-    for t in range(n, 0, -1):
+    for t in range(n, 0, -1): #index through all nodes
         s = r[t]
         if avisited[s]:
             continue
@@ -123,7 +125,7 @@ def slashburn(A, k=None, greedy=True):
     iteration = 0
     while True:
         iteration += 1
-        if greedy:
+        if greedy: #decrements after removing before next iteration of computing max degree
             for _ in range(k): # remove highest degree vertex k times
                 if not len(degree):
                     break
@@ -135,7 +137,7 @@ def slashburn(A, k=None, greedy=True):
                 for target in alist:
                     if target in degree:
                         degree[target] -= 1
-        else:
+        else: #top k degrees are identified at the beginning
             tops = sorted(degree, key=degree.get, reverse=True)[:k]
             head.extend(tops)
             for top in tops:
@@ -153,11 +155,11 @@ def slashburn(A, k=None, greedy=True):
             for target in alists[source]:
                 if target in alists:
                     edges.append((source, target))
-        ccs = scc(nodes, edges) # Generates list of subgraphs left after hub removal
+        ccs = scc(nodes, edges) #list of all strongly connected nodes
         m = len(ccs)
-        sizes = {i: len(ccs[i]) for i in range(m)}
+        sizes = {i: len(ccs[i]) for i in range(m)} #number of neighbors?
         ordering = sorted(sizes, key=sizes.get)
-        ccs = [ccs[ordering[i]] for i in range(m)]
+        ccs = [ccs[ordering[i]] for i in range(m)] #unsure of what this ends up storing
         # todo: implement hub-ordering
         for cc in ccs:
             size = len(cc)
@@ -170,7 +172,7 @@ def slashburn(A, k=None, greedy=True):
                     for target in alist:
                         if target in degree:
                             degree[target] -= 1
-        assert len(head) + len(tail) + len(degree) == n
+        assert len(head) + len(tail) + len(degree) == n #check to make sure numbers make sense (assert is a debugging tool)
         if not len(degree):
             break
     tops = tail + head[::-1]
@@ -182,7 +184,7 @@ def slashburn(A, k=None, greedy=True):
 # Test Graph 
 A = np.array([[1, 1, 0, 0], [1, 1, 1, 1], [0, 1, 1, 0], [0, 1, 0, 1]], dtype=np.int32)
 A = coo_matrix(A)
-print(verbose_matrix(A))
+#print(verbose_matrix(A))
 """
 1 1
 1 1 1 1
@@ -191,8 +193,9 @@ print(verbose_matrix(A))
 """
 perm, wing = slashburn(A)
 print(wing)  # 1
+print(perm)
 A = reorder_matrix(A, perm)
-print(verbose_matrix(A))
+#print(verbose_matrix(A))
 """
 1     1
   1   1
